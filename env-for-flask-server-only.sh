484444a810907afc8c9b5a1fed896baa84a0e7ef
Brandon Krull <btkrull@gmail.com>
2018-07-30 18:54:29 -0700

    updating frontend with placeholder data/forms

---
+++ env-for-flask-server-only.sh
@@ -0,0 +1,4 @@
+export FLASK_APP=./redditonomy/__init__.py
+export FLASK_DEBUG=True
+
+flask run --port=5001
--- redditonomy/views.py
+++ redditonomy/views.py
@@ -4,4 +4,4 @@ from flask import request, session, g, redirect, \
 
 @app.route('/')
 def home():
-    return render_template('def.html')
+    return render_template('home.html')
