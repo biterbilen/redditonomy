e3ffcb302170960b8d50e36465df31cbaeebdc13
Brandon Krull <btkrull@gmail.com>
2018-08-02 14:03:16 -0700

    adding basic postgres call

--- redditonomy/views.py
+++ redditonomy/views.py
@@ -1,7 +1,49 @@
+import os
+from ConfigParser import ConfigParser
+from sqlalchemy import create_engine, Column, String, Integer, Date
+from sqlalchemy.dialects import postgresql
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 from redditonomy import app
 from flask import request, session, g, redirect, \
     url_for, render_template, send_from_directory, jsonify
 
+creds = ConfigParser()
+creds.read(os.path.expanduser('~/.aws/dbcreds'))
+
+user = creds.get('db', 'user')
+pwd = creds.get('db', 'password')
+ip = creds.get('db', 'ip')
+port = creds.get('db', 'port')
+table = creds.get('db', 'table')
+db = 'postgresql://{user}:{pwd}@{ip}:{port}/{table}'.format(
+    user=user,
+    pwd=pwd,
+    ip=ip,
+    port=port,
+    table=table)
+
+engine = create_engine(db, echo=True)
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
+
+class Results(Base):
+    __tablename__ = 'results'
+    extend_existing = True
+
+    id = Column(Integer, primary_key=True)
+    subreddit = Column(String)
+    date = Column(Date)
+    results = Column(postgresql.JSONB())
+
+    def __init__(self, subreddit, date, results):
+        self.data = results
+
+Base.metadata.create_all(engine)
+sess = Session()
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    query = sess.query(Results).all()
+
+    return render_template('home.html', data=query[0].results)
