1eabc6a148c7ddb3c1e118fef3f7a4f2ae9823ba
Brandon Krull <btkrull@gmail.com>
2018-08-12 08:56:17 -0700

    removing console logs from tax.js

--- redditonomy/static/taxonomy.js
+++ redditonomy/static/taxonomy.js
@@ -53,7 +53,6 @@ $(document).ready(function() {
             $(document).data('subreddit', subreddit);
 
             var url = "http://127.0.0.1:5001/q/" + subreddit;
-            console.log(url);
             $.ajax({
                 url: url,
                 dataType: 'json',
@@ -64,8 +63,6 @@ $(document).ready(function() {
             });
 
             var results = $(document).data('results');
-            console.log(subreddit);
-            console.log(results[0]);
             $('#subreddit').text('/r/' + subreddit);
 
             $('#date').text('week of ' + results[0]['date']);
@@ -82,6 +79,44 @@ $(document).ready(function() {
         } else {
             $('#subreddit').text();
             $('#date').text();
+        }
+    });
+
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
         }
     });
 });
