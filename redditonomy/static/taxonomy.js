36a009936967f86dd8557f8cc3cac6dddbaa98fc
Brandon Krull <btkrull@gmail.com>
2018-08-12 11:56:30 -0700

    tax.js now adds chart when results are received

--- redditonomy/static/taxonomy.js
+++ redditonomy/static/taxonomy.js
@@ -22,9 +22,6 @@ function initialize_slider() {
         handleClass: 'rangeslider__handle',
 
         // Callback function
-        onInit: function() {},
-
-        // Callback function
         onSlide: function(position, value) {
             var $handle = this.$range.find('.rangeslider__handle__value');
             var results = $(document).data('results');
@@ -39,7 +36,53 @@ function initialize_slider() {
         },
 
         // Callback function
+        onInit: function() {},
+
+        // Callback function
         onSlideEnd: function(position, value) {}
+    });
+}
+
+function build_chart(results) {
+    $('<div>').addClass(
+        'mui-col-sm-6 mui-col-sm-offset-3'
+    ).append(
+        $('<canvas>').attr({
+            id: 'chart',
+            width: 600,
+            height: 400
+        })
+    ).appendTo('#results');
+
+    var blue = 'rgba(33, 150, 243,0.2)';
+    var green = 'rgba(99, 210, 151, 0.2)';
+    var newChart = new Chart($('#chart'), {
+        type: 'line',
+        data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    green,
+                    blue,
+                ],
+                borderColor: [
+                    blue,
+                    green,
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
     });
 }
 
@@ -76,47 +119,10 @@ $(document).ready(function() {
             $('#rangeslider-wrapper').empty()
                 .html('<input type="range" value="0" min="0" max="' + numweeks + '" step="1">');
             initialize_slider();
+            build_chart(results);
         } else {
             $('#subreddit').text();
             $('#date').text();
-        }
-    });
-
-    var ctx = document.getElementById("myChart").getContext('2d');
-    var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:true
-                    }
-                }]
-            }
         }
     });
 });
