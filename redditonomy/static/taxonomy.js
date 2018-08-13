0ad9f31d51977fb8032f92cd9cbf0b2bae29fbcb
Brandon Krull <btkrull@gmail.com>
2018-08-13 09:26:41 -0700

    fixing incorrect js: stop adding new elems

--- redditonomy/static/taxonomy.js
+++ redditonomy/static/taxonomy.js
@@ -1,42 +1,50 @@
 "use strict";
 
-function build_taxonomy_table(results, name) {
-    $('<div>').addClass(
-        'mui-col-sm-4 mui-col-md-4 mui-col-lg-4'
-    ).append(
+function build_taxonomy_table(name) {
+    $('#results').html(
         $('<div>').addClass(
-            'mui-panel'
-        ).css({
-            'padding': '50px'
-        }).append(
-            $('<div>').addClass(
-                'mui-row'
-            ).append(
-                $('<div>').addClass(
-                    'mui-col-sm-12'
-                ).append(
-                    $('h2').text(name)
-                )
-            )
+            'mui-col-sm-4 mui-col-md-4 mui-col-lg-4'
         ).append(
-            $('<table>').addClass(
-                'mui-table mui-table--bordered'
-            ).attr(
-                'id', 'taxonomy'
-            ).append(
-                $('<thead>').append(
-                    $('<tr>').append(
-                        $('<th>').text('term')
+            $('<div>').addClass(
+                'mui-panel'
+            ).css({
+                'padding': '50px'
+            }).append(
+                $('<div>').addClass(
+                    'mui-row'
+                ).append(
+                    $('<div>').addClass(
+                        'mui-col-sm-12'
                     ).append(
-                        $('<th>').text('score')
+                        $('h2').text(name)
+                    ).append(
+                        $('h3').addClass(
+                            'mui--text-accent-secondary'
+                        ).text(name)
                     )
                 )
             ).append(
-                $('<tbody>')
+                $('<table>').addClass(
+                    'mui-table mui-table--bordered'
+                ).attr(
+                    'id', 'taxonomy'
+                ).append(
+                    $('<thead>').append(
+                        $('<tr>').append(
+                            $('<th>').text('term')
+                        ).append(
+                            $('<th>').text('score')
+                        )
+                    )
+                ).append(
+                    $('<tbody>')
+                )
             )
         )
-    ).appendTo('#results');
+    );
+}
 
+function fill_taxonomy_table(results) {
     for (var i = 0; i < results.length; i++) {
         $('#taxonomy tbody')
             .append($('<tr>')
@@ -44,39 +52,6 @@ function build_taxonomy_table(results, name) {
                 .append($('<td>').text(results[i][i]['_2']))
             );
     }
-}
-
-function initialize_slider() {
-    $('input[type="range"]').rangeslider({
-        polyfill: false,
-
-        // Default CSS classes
-        rangeClass: 'rangeslider',
-        disabledClass: 'rangeslider--disabled',
-        horizontalClass: 'rangeslider--horizontal',
-        fillClass: 'rangeslider__fill',
-        handleClass: 'rangeslider__handle',
-
-        // Callback function
-        onSlide: function(position, value) {
-            var $handle = this.$range.find('.rangeslider__handle__value');
-            var results = $(document).data('results');
-            var thisEntry = results[this.value];
-
-            $('#date').text('week of ' + thisEntry['date']);
-            $('#taxonomy tbody').empty();
-            build_taxonomy_table(thisEntry['results']);
-
-            $('#vocab').text('Vocab size: ' + thisEntry['vocab_size']);
-            $('#documents').text('Number of documents: ' + thisEntry['num_docs']);
-        },
-
-        // Callback function
-        onInit: function() {},
-
-        // Callback function
-        onSlideEnd: function(position, value) {}
-    });
 }
 
 function build_chart(results) {
@@ -153,8 +128,6 @@ function build_chart(results) {
 }
 
 $(document).ready(function() {
-    initialize_slider();
-
     $('#subreddit-selector').on('change', function() {
         if (this.value) {
             var subreddit = this.value;
@@ -175,8 +148,8 @@ $(document).ready(function() {
             $('#subreddit').text('/r/' + subreddit);
 
             $('#date').text('week of ' + results[0]['date']);
-            $('#taxonomy tbody').empty();
-            build_taxonomy_table(results[0]['results'], 'online');
+            build_taxonomy_table('online');
+            fill_taxonomy_table(results[0]['results']);
 
             $('#vocab').text('Vocab size: ' + results[0]['vocab_size']);
             $('#documents').text('Number of documents: ' + results[0]['num_docs']);
@@ -184,7 +157,6 @@ $(document).ready(function() {
             var numweeks = results.length - 1;
             $('#rangeslider-wrapper').empty()
                 .html('<input type="range" value="0" min="0" max="' + numweeks + '" step="1">');
-            initialize_slider();
             build_chart(results);
         } else {
             $('#subreddit').text();
