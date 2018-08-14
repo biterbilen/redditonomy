8e32ffc36b61bcfb44f469d7fc6db16b32a37218
Brandon Krull <btkrull@gmail.com>
2018-08-14 12:05:21 -0700

    yapifying preprocess.py

--- src/spark/preprocess-to-s3.py
+++ src/spark/preprocess-to-s3.py
@@ -16,18 +16,17 @@ if __name__ == '__main__':
     config = configparser.ConfigParser()
 
     config.read(os.path.expanduser('~/.aws/credentials'))
-    access_id = config.get('aws', 'aws_access_key_id') 
+    access_id = config.get('aws', 'aws_access_key_id')
     access_key = config.get('aws', 'aws_secret_access_key')
 
     dbuser = config.get('db', 'user')
     dbpwd = config.get('db', 'password')
-    dbip= config.get('db', 'ip')
+    dbip = config.get('db', 'ip')
     dbport = config.get('db', 'port')
     dbname = config.get('db', 'database')
 
-    dburl = 'jdbc:postgresql://{ip}:{port}/{database}'.format(ip=dbip,
-                                                              port=dbport,
-                                                              database=dbname)
+    dburl = 'jdbc:postgresql://{ip}:{port}/{database}'.format(
+        ip=dbip, port=dbport, database=dbname)
 
     os.environ['PYSPARK_SUBMIT_ARGS'] = \
             "--packages org.apache.hadoop:hadoop-aws:2.7.1 pyspark-shell"
@@ -36,12 +35,12 @@ if __name__ == '__main__':
             .appName("S3 READ TEST") \
             .config("spark.executor.cores", "6") \
             .config("spark.executor.memory", "6gb") \
-	    .config("spark.sql.session.timeZone", "America/Los_Angeles") \
+     .config("spark.sql.session.timeZone", "America/Los_Angeles") \
             .getOrCreate()
 
-    sc=spark.sparkContext
+    sc = spark.sparkContext
 
-    hadoop_conf=sc._jsc.hadoopConfiguration()
+    hadoop_conf = sc._jsc.hadoopConfiguration()
     hadoop_conf.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
     hadoop_conf.set("fs.s3a.awsAccessKeyId", access_id)
     hadoop_conf.set("fs.s3a.awsSecretAccessKey", access_key)
@@ -56,8 +55,8 @@ if __name__ == '__main__':
         StructField('author', StringType()),
         StructField('author_flair_css_class', StringType()),
         StructField('author_flair_text', StringType()),
-        StructField('body', StringType()), 
-        StructField('controversiality', IntegerType()), 
+        StructField('body', StringType()),
+        StructField('controversiality', IntegerType()),
         StructField('created_utc', StringType()),
         StructField('distinguished', StringType()),
         StructField('downs', IntegerType()),
@@ -79,9 +78,9 @@ if __name__ == '__main__':
          for word in x.lower().split() \
          if (len(word) > 3 and \
               word.isalpha() and \
-             word not in stopwords)], 
+             word not in stopwords)],
         ArrayType(StringType()))
-        
+
     for year in range(2008, 2016):
         for month in range(1, 13):
             print 'Reading {year}: {month:02d}'.format(year=year, month=month)
@@ -97,17 +96,17 @@ if __name__ == '__main__':
             df = df.drop(*columns_to_drop) \
                  .filter(df.score > 10) \
                  .filter(df.body != '[deleted]')
-            
-            df = df.withColumn('created_utc', 
-                           df.created_utc.cast(IntegerType()))
-            df = df.withColumn('created_utc', 
-                 	       from_unixtime(df.created_utc, format='yyyy-MM-dd'))
-            
+
+            df = df.withColumn('created_utc',
+                               df.created_utc.cast(IntegerType()))
+            df = df.withColumn('created_utc',
+                               from_unixtime(
+                                   df.created_utc, format='yyyy-MM-dd'))
+
             df = df.withColumn('body', udf_tokenize('body')) \
                  .filter(size('body') != 0)
-        
+
             print 'Writing {year}: {month}'.format(year=year, month=month)
             df.write.json(outfilepath.format(year=year, month=month))
 
             spark.catalog.clearCache()
-
